ac11f3dbeb33bf6d4612301da451185471c50fe4
Marcos-Dev7 <markimdvp@gmail.com>
2026-03-14 09:59:11 -0300

    chore: initial project setup with spring boot44.0 and java 25

---
+++ src/main/java/com/marcosdev7/financas/FinancasInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.marcosdev7.financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancasInvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosdev7/financas/FinancasInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosdev7.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
