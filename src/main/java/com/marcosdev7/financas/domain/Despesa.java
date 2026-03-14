aa4fb0cd1409453b4ae0ba199412551aaed65699
Marcos-Dev7 <markimdvp@gmail.com>
2026-03-14 11:12:42 -0300

    chore: use environment variables for database credentials

--- src/main/java/com/marcosdev7/financas/domain/Despesa.java
+++ src/main/java/com/marcosdev7/financas/domain/Despesa.java
@@ -1,5 +1,6 @@
 package com.marcosdev7.financas.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -9,10 +10,15 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-
+@Entity
+@Table(name = "despesas")
 public class Despesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String descricao;
+    @Enumerated(EnumType.STRING)
     Mes mes;
     BigDecimal valor;
 }
