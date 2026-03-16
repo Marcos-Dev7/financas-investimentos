5eb6658e80cab944792ec537c05e079a296feb06
Marcos-Dev7 <markimdvp@gmail.com>
2026-03-16 15:45:13 -0300

    feat: standardizing API responses with ResponseEntity and proper HTTP status codes

--- src/main/java/com/marcosdev7/financas/controller/DespesaController.java
+++ src/main/java/com/marcosdev7/financas/controller/DespesaController.java
@@ -3,6 +3,7 @@ package com.marcosdev7.financas.controller;
 import com.marcosdev7.financas.domain.Despesa;
 import com.marcosdev7.financas.repository.DespesaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,25 +16,28 @@ public class DespesaController {
     private DespesaRepository despesaRepository;
 
     @GetMapping
-    public List<Despesa> listarDespesar(){
-        return despesaRepository.findAll();
+    public ResponseEntity<List<Despesa>> listarDespesar(){
+        var lista = despesaRepository.findAll();
+        return ResponseEntity.ok(lista);
     }
 
     @PostMapping
-    public Despesa cadastrarDespesa(@RequestBody Despesa despesa) {
-        return despesaRepository.save(despesa);
+    public ResponseEntity<Void> cadastrarDespesa(@RequestBody Despesa despesa) {
+        despesaRepository.save(despesa);
+        return ResponseEntity.status(201).build();
     }
 
     @PutMapping("/{id}")
-    public Despesa atualizarDespesa(@PathVariable Long id, @RequestBody Despesa despesa){
+    public ResponseEntity<Despesa> atualizarDespesa(@PathVariable Long id, @RequestBody Despesa despesa){
         despesaRepository.findById(id).orElseThrow();
         despesa.setId(id);
-        return despesaRepository.save(despesa);
+        return ResponseEntity.ok(despesaRepository.save(despesa));
     }
 
     @DeleteMapping("/{id}")
-    public void ApagarDespesa(@PathVariable Long id){
+    public ResponseEntity<Void> ApagarDespesa(@PathVariable Long id){
         var buscarDespesa = despesaRepository.findById(id).orElseThrow();
         despesaRepository.deleteById(buscarDespesa.getId());
+        return ResponseEntity.noContent().build();
     }
 }
