867bcbddb9e6511704af3bad090d3d8b4213a25b
Marcos-Dev7 <markimdvp@gmail.com>
2026-03-16 16:00:23 -0300

    feat: implement global exception handling for 404 errors

---
+++ src/main/java/com/marcosdev7/financas/exceptions/TratamentoDeErros.java
@@ -0,0 +1,17 @@
+package com.marcosdev7.financas.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class TratamentoDeErros {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+}
